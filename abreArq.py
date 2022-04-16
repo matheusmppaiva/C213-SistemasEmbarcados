3d7b3f42446af81c423d36712e65c775b44433a5
matheusmppaiva <matheus.paiva@gec.inatel.br>
2022-04-16 14:52:02 -0300

    Abrindo arq do matlab

---
+++ abreArq.py
@@ -0,0 +1,9 @@
+import scipy.io
+
+def abreArquivo():
+
+    arq = scipy.io.loadmat('dadosgrupo6.mat')
+
+    return arq
+
+
---
+++ funcaoTransf.py
@@ -0,0 +1,10 @@
+import abreArq
+
+
+def calculaFuncao(maxPico, TempAcom, Tolerancia):
+    
+    arq = abreArq.abreArquivo()
+    
+
+
+
